54f9009417563c277f71aba117161288b72616e9
Alma <abecerrilsalas@gmail.com>
2024-07-28 18:47:06 -0700

    oops

--- src/index.js
+++ src/index.js
@@ -15,14 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css"; // Optional, for your CSS if you have global styles
-// import App from "./App"; // This assumes you have an App.js component
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
