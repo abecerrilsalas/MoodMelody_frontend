21e7d4baedabcaa8d455bee511c986836bb2ad33
Alma <abecerrilsalas@gmail.com>
2024-08-06 22:26:20 -0700

    missing footer import fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Playlist from "./Playlist";
 import About from "./About";
 import Header from "./Header";
+import Footer from "./Footer";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/playlist" element={<Playlist />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
