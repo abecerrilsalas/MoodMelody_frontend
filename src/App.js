9c922afa97741b6431c7d3d5bd345dfe30d4055f
Alma <abecerrilsalas@gmail.com>
2024-08-07 17:52:58 -0700

    changed About and updated url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Playlist from "./Playlist";
-import About from "./About";
+import Landing from "./Landing";
 import Header from "./Header";
 import Footer from "./Footer";
 import "./App.css";
@@ -13,7 +13,7 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<About />} />
+          <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
           <Route path="/playlist" element={<Playlist />} />
         </Routes>
